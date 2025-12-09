62dc1cb15dcc7751a1d8c58a24644f4025b628a0
Nikhil Vyas <nikhilvyas891@gmail.com>
2025-12-09 13:55:31 +0530

    Update RedisConfig.java

--- multi-account-gcp/worker/src/main/java/com/example/multiaccount/worker/config/RedisConfig.java
+++ multi-account-gcp/worker/src/main/java/com/example/multiaccount/worker/config/RedisConfig.java
@@ -1,1 +1,26 @@
+package com.example.multiaccount.worker.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(System.getenv().getOrDefault("SPRING_REDIS_HOST", "redis"));
+        config.setPort(Integer.parseInt(System.getenv().getOrDefault("SPRING_REDIS_PORT", "6379")));
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
